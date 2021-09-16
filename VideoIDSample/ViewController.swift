6ba588459f5210eaef3e7be4e688d70b645b9b62
Cesar Sosa <cesarsosa.p@gmail.com>
2021-09-16 13:17:08 +0200

    init commit

---
+++ VideoIDSample/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  VideoIDSample
+//
+//  Created by César Sosa on 16/9/21.
+//
+
+import UIKit
+import VideoIDSDK
+
+class ViewController: UIViewController {
+
+    let endpoint = "https://etrust-sandbox.electronicid.eu/v2"
+    let bearer = "b92ab9ae-40a7-49c1-b635-f6b1b6e0116d"
+    let rAuthority = ""
+    let language = "en"
+    let documentID = 62 //Spain ID
+    var getVideoIDAuthorizationInteractor: GetVideoIDAuthorizationInteractor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        getVideoIDAuthorizationInteractor = GetVideoIDAuthorizationInteractor()
+    }
+
+    
+    @IBAction func startVideo(_ sender: Any) {
+        
+        getVideoIDAuthorizationInteractor?.getAuthorizationFor(endpoint: self.endpoint,
+                                                               bearer: self.bearer,
+                                                               rAuthority: self.rAuthority,
+                                                               onResult: { (auth) in
+                                                                
+                                                                
+                                                                let environment: VideoIDSDK.SDKEnvironment = VideoIDSDK.SDKEnvironment(url: self.endpoint, authorization: auth!)
+                                                                
+                                                                DispatchQueue.main.async {
+                                                                    
+                                                                    let view = VideoIDSDK.VideoIDSDKViewController(environment: environment, docType:self.documentID, language: self.language)
+                                                                    view.modalPresentationStyle = .fullScreen
+                                                                    view.delegate = self
+                                                                    self.present(view,
+                                                                                 animated: true, completion: nil)
+                                                                    
+                                                                }
+                                                                
+                                                               }) { (error) in
+            self.onError(code: "Error", message: "No se pudo crear autorizacion")
+        }
+    }
+    
+}
+extension ViewController : VideoIDSDK.VideoIDDelegate{
+    func onComplete(videoID: String) {
+        DispatchQueue.main.async {
+            
+            let alert = UIAlertController(title: "Success", message: "VideoID: \(videoID)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func onError(code: String, message: String?) {
+        
+            DispatchQueue.main.async {
+                
+                let alert = UIAlertController(title: "Error", message: "Message: \(message!)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true)
+            }
+    }
+    
+    
+}
